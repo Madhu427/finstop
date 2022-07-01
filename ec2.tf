99d436f26ab9db0bc878fd16ddaa9ec12a65913e
Madhu427 <madhusudhanachary.k@gmail.com>
2022-07-01 22:40:09 +0530

    ec2 added

---
+++ ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "sample" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t2.micro"
+  count         =  2
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  }
+
+output "out"{
+  value = aws_instance.sample.public_ip
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
